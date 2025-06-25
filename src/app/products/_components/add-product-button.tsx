9fc7f4425f78b56a04e54f64359889d370531254
marco valadares <marcovf99@gmail.com>
2025-06-25 17:54:07 -0300

    feat: adiciona botão para criar novos produtos e implementa formulário com validação usando Zod e React Hook Form

---
+++ src/app/products/_components/add-product-button.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { NumericFormat } from "react-number-format";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome do produto é obrigatório.",
+  }),
+  price: z.number().min(0.01, {
+    message: "O preço do produto é obrigatório.",
+  }),
+  stock: z.coerce
+    .number()
+    .positive({
+      message: "A quantidade de estoque deve ser positiva",
+    })
+    .int()
+    .min(0, {
+      message: "A quantidade é obrigatório.",
+    }),
+});
+
+const AddProductButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    shouldUnregister: true,
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      stock: 1,
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log({ data });
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="text-primary-foreground hover:bg-primary/70">
+          <PlusIcon className="h-4 w-4" />
+          Novo produto
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <DialogHeader>
+              <DialogTitle>Criar Produto</DialogTitle>
+              <DialogDescription>Insira as informações</DialogDescription>
+            </DialogHeader>
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome do Produto</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome do produto" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Preço do Produto</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      thousandSeparator="."
+                      decimalSeparator=","
+                      fixedDecimalScale
+                      decimalScale={2}
+                      prefix="R$"
+                      allowNegative={false}
+                      customInput={Input}
+                      onValueChange={(values) =>
+                        field.onChange(values.floatValue)
+                      }
+                      {...field}
+                      onChange={() => {}}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Estoque</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Digite o estoque do produto"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="reset" variant="secondary">
+                  Cancelar
+                </Button>
+              </DialogClose>
+              <Button type="submit">Salvar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddProductButton;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@/components/ui/button";
 
-import { PlusIcon } from "lucide-react";
 import { productTableColumns } from "./_components/table-columns";
 import { DataTable } from "../_components/data-table";
 import { getProducts } from "../_data-acess/product/get-products";
+import AddProductButton from "./_components/add-product-button";
 
 const ProductsPage = async () => {
-  const products = await getProducts()
+  const products = await getProducts();
   return (
-    <div className="m-8 w-full space-y-8 bg-white p-8 rounded-lg">
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
       <div className="flex w-full items-center justify-between">
         <div className="space-y-1">
           <span className="text-muted-foreground text-xs font-semibold">
@@ -16,12 +15,12 @@ const ProductsPage = async () => {
           </span>
           <h1 className="text-2xl font-bold">Produtos</h1>
         </div>
-        <Button className="text-primary-foreground hover:bg-primary/70">
-          <PlusIcon className="h-4 w-4" />
-          Novo produto
-        </Button>
+       <AddProductButton/>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))}
+      />
     </div>
   );
 };
