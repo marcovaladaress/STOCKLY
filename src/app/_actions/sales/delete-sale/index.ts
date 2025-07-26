154908663143c840be3a482fda7058607288a591
marco valadares <marcovf99@gmail.com>
2025-07-26 18:22:40 -0300

    feat: aprimora a funcionalidade de exclusão de vendas, incluindo verificação de existência da venda e atualização do estoque dos produtos relacionados

--- src/app/_actions/sales/delete-sale/index.ts
+++ src/app/_actions/sales/delete-sale/index.ts
@@ -8,10 +8,33 @@ import { revalidatePath } from "next/cache";
 export const deleteSale = actionClient
   .schema(deleteSaleSchema)
   .action( async ({ parsedInput: { id } }) => {
+    const sale = await db.sale.findUnique({
+      where:{
+        id,
+      },
+      include:{
+        products: true
+      }
+    });
+    if(!sale) return
     await db.sale.delete({
         where:{
             id
         }
     })
+    for (const product of sale.products){
+      await db.product.update({
+        where:{
+          id:product.productId,
+        },
+        data:{
+          stock:{
+            increment: product.quantity
+          }
+        }
+      })
+    }
     revalidatePath("/sales")
+    revalidatePath("/products")
+    revalidatePath("/")
   });
